f8334493fa00fdb113881e917b6a9c66336b1a26
thomasdriscoll <thomasdriscoll98@gmail.com>
2021-01-23 13:29:19 -0600

    Updated Dockerfile port and configureed to work with remote config server

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,7 +44,9 @@ dependencies {
 
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.cloud:spring-cloud-commons:3.0.0")
+    implementation("org.springframework.cloud:spring-cloud-starter-config:3.0.0")
+    implementation("org.springframework.cloud:spring-cloud-starter-bootstrap:3.0.0")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
